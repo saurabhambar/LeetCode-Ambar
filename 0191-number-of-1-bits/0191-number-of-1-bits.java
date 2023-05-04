395f5b882fb49f511d07d826ce08aa1e5c3eb64e
SAURABH AMBAR <saurabh.ambar2017@gmail.com>
2023-05-04 19:39:00 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (85.77%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -11,12 +11,20 @@ public class Solution {
     //     return count;
     // }
     
+    // public int hammingWeight(int n){
+    //     int count =0;
+    //     while(n != 0 ){
+    //         n = n & (n-1);
+    //         count++;
+    //     }
+    //     return count;
+    // }
+    
     public int hammingWeight(int n){
-        int count =0;
-        while(n != 0 ){
-            n = n & (n-1);
-            count++;
-        }
-        return count;
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        
+        return hammingWeight(n & (n-1)) + 1;
     }
+    
 }
