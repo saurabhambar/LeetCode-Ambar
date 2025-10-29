5f9d0afa74deaba33669fe4e8b328a3174e7e8bf
SAURABH AMBAR <saurabh.ambar2017@gmail.com>
2025-10-30 02:01:16 +0530

    Time: 16 ms (43.1%), Space: 20.7 MB (62.27%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_map = defaultdict(list)
+    
+        for s in strs:
+            key = ''.join(sorted(s))
+            anagram_map[key].append(s)
+        
+        return list(anagram_map.values())
